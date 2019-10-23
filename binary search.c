24b120337bf00f2d590c132733cffbf8f35e691d
Gourangakataky18070123041 <54322520+Gourangakataky18070123041@users.noreply.github.com>
2019-10-23 08:43:20 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -5,50 +5,40 @@ void sort(int *a,int n)
 int c,x, temp,flag=1;
  temp = 0;
  while(flag == 1)
- {flag = 0;
- for (c = 1; c < n; c++)
-      {
-	  if (a[temp]>=a[c])
-       {
-	   x = a [c];
-       a[c] = a [temp];
-       a[temp] = x;
-       temp++;
-       flag == 1;
-  }
-      else
-       temp++;
+ {
+	 flag = 0;
+ 	for (c = 1; c < n; c++)
+      	{
+	  	if (a[temp]>=a[c])
+       		{
+	   		x = a [c];
+       			a[c] = a [temp];
+       			a[temp] = x;
+       			temp++;
+       			flag == 1;
+  		}
+      		else
+       		temp++;
       
-  }
-     
-	 for (c = 0; c < n; c++)
-      printf("%d\n",a[c]);
+  	}
+     	for (c = 0; c < n; c++)
+      	printf("%d\n",a[c]);
+ }
 }
-
-}
-
 int main()
-
 {
    int c, first, last, middle, n, search, array[100];
- 
    printf("Enter number of elements\n");
    scanf("%d",&n);
- 
    printf("Enter %d integers\n", n);
- 
    for (c = 0; c < n; c++)
-      scanf("%d",&array[c]);
- 
+   scanf("%d",&array[c]);
    printf("Enter value to find\n");
    scanf("%d", &search);
-   
    sort(array,n);
- 
    first = 0;
    last = n - 1;
    middle = (first+last)/2;
- 
    while (first <= last) {
       if (array[middle] < search)
          first = middle + 1;    
